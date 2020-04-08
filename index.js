3d0e53b9aea10e855a85d656c9c616c2fdc63263
Hakan Yildiz <hakan@re-coded.com>
2020-04-08 10:53:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,14 @@ function reduce(arr,callback,startingPoint=null){
   //inside callback we will pass acc curr
   let value1=startingPoint;
   for(let element of arr){
-    value1=callback(element)
+    value1=value1+element
+  }
+  for(let element of arr){
+    if(element){
+      return true;
+    }else{
+      return false;
+    }
   }
   return value1;
 }
